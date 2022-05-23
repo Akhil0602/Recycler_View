aa54e38cb83c7065f312408259b510a1a819d7ed
akhil <am.akhilmahajan@gmail.com>
2022-05-23 15:49:49 +0530

    commit

--- app/src/main/java/com/example/recyclerview/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/recyclerview/RecyclerViewAdapter.kt
@@ -8,18 +8,32 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class RecyclerViewAdapter(contex: Context) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
-    var arr= mutableListOf<String>("Akhil","RAM","Monish","Bharat","Anand")
-    var cn= mutableListOf<String>("123456789","123456789","123456789","123456789","123456789")
-    var context:Context=contex
+var arr = mutableListOf<String>("Akhil", "RAM", "Monish", "Bharat", "Anand")
+var cn = mutableListOf<String>("123456789", "123456789", "123456789", "123456789", "123456789")
+interface OnClickListener
+{
+    fun onRemove(context:Context)
+    {
+        /*arr.removeAt(id)
+        cn.removeAt(id)*/
+        Toast.makeText(context,"Clicked Name",Toast.LENGTH_SHORT).show()
+    }
+    fun onEdit()
+}
+
+class RecyclerViewAdapter(contex: Context) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>(),OnClickListener {
+
+
+    var context: Context = contex
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v=LayoutInflater.from(parent.context).inflate(R.layout.format,parent,false)
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.format, parent, false)
         return ViewHolder(v)
     }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.Tname.text=cn[position]
-        holder.Tcn.text=arr[position]
+        holder.Tname.text = cn[position]
+        holder.Tcn.text = arr[position]
     }
 
 
@@ -27,25 +41,25 @@ class RecyclerViewAdapter(contex: Context) : RecyclerView.Adapter<RecyclerViewAd
         return arr.size
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),View.OnClickListener
-    {
-        var Tcn =itemView.findViewById<TextView>(R.id.name)
-        var Tname =itemView.findViewById<TextView>(R.id.contact)
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),View.OnClickListener {
+        var Tcn = itemView.findViewById<TextView>(R.id.name)
+        var Tname = itemView.findViewById<TextView>(R.id.contact)
+
         init {
-            Tcn.setOnClickListener{
-                var pos=this.absoluteAdapterPosition
-                Toast.makeText(context,"You clicked on name "+ pos.toString(),Toast.LENGTH_SHORT).show()
-            }
-            Tname.setOnClickListener{
-                Toast.makeText(context,"This is contact",Toast.LENGTH_SHORT).show()
-            }
-            itemView.setOnClickListener(this)
+            var pos = this.absoluteAdapterPosition
+            Tcn.setOnClickListener{onRemove(context)}
+            Toast.makeText(context, "You clicked on name " + pos.toString(), Toast.LENGTH_SHORT).show()
         }
 
         override fun onClick(p0: View?) {
-            Toast.makeText(context,"Hello",Toast.LENGTH_SHORT).show()
+
         }
+    }
+
+    override fun onEdit() {
 
     }
 
+
 }
+
